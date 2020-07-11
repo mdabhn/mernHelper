e62a7f09c8b35898d213466d9ff3e624dbe01988
mdabhn <mdabhn@gmail.com>
2020-07-11 11:38:43 +0600

    routing mongoo connection added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const databaseConnection = require('./config/database')
+databaseConnection()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+
+/** @Routing */
+
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/user', require('./routes/api/user'))
+app.use('/api/post', require('./routes/api/post'))
+app.use('/api/profile', require('./routes/api/profile'))
---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+const databaseConnection = async () => {
+  try {
+    await mongoose.connect(config.get('mongoURI'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = databaseConnection
